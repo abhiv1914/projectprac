81223939769b557bad39cdde2b906b00135d4c46
Abhinav Verma <abhinaverma1495@gmail.com>
2023-06-16 20:31:11 +0530

    contact

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import './contact.css';
+const Contact = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section id="header">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top mb-1">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="1.html">
+              <img
+                src="logo.png"
+                alt="Logo"
+                className="img-fluid"
+                style={{ maxHeight: '35px' }}
+              />
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav ms-auto">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="1.html">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="login.html">Login</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="Products.html">Products</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="review.html">Reviews</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="contact.html"><b>Contact us</b></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </section>
+
+      <section id="contact">
+        <div className="contact">
+          <div className="container">
+            <div className="mb-5 text-center">
+              <h5>Let's Start a Conversation</h5>
+              <h2 className="fw-bold">Contact Us</h2>
+            </div>
+
+            <div className="row">
+              <div className="col-lg-5 col-md-5">
+                <h4 className="fw-bold">Contact Info</h4>
+                <ul className="info list-unstyled">
+                  <li className="d-flex align-items-center">
+                    <span className="pe-3 ti-location-pin fs-5"></span>
+                    <p><a href="#" className="text-info">Lorem ipsum dolor sit amet.</a></p>
+                  </li>
+                  <li className="d-flex align-items-center">
+                    <span className="pe-3 ti-mobile fs-5"></span>
+                    <p><a href="#" className="text-info">+91 999-999-9999</a></p>
+                  </li>
+                  <li className="d-flex align-items-center">
+                    <span className="pe-3 ti-envelope fs-5"></span>
+                    <p><a href="#" className="text-info">Info@carkharido.in</a></p>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-lg-7 col-md-7 pt-lg-0 pt-md-0 pt-4">
+                <form>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <div className="form-group">
+                        <input type="text" className="form-control" name="name" id="name" placeholder="Your name" />
+                      </div>
+                    </div>
+                    <div className="col-md-6">
+                      <div className="form-group">
+                        <input type="email" className="form-control" name="email" id="email" placeholder="Enter address" />
+                      </div>
+                    </div>
+                    <div className="col-md-12">
+                      <div className="form-group">
+                        <textarea className="textarea" name="message" id="message" cols="30" rows="4" placeholder="Enter your message"></textarea>
+                      </div>
+                    </div>
+                    <div className="col-md-12">
+                      <button className="btn btn-danger"><span className="ti-rocket pe-2 fs-5"></span>Send Message</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <footer>
+        <div className="p-3 copyright">
+          <div className="container">
+            <div className="row align-items-center">
+              <div className="col-12 col-lg-6 p-1 p-lg-3 text-center text-lg-start">
+                <p className="my-0">Copyright © 2023 <a href="#">CAR_KHARIDO</a> All Rights Reserved</p>
+              </div>
+              <div className="col-12 col-lg-6 p-1 p-lg-3 text-center text-lg-end">
+                <p>Designed by <a href="#" target="_blank">Team69</a>.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   );
-}
+};
 
-export default App;
+export default Contact;
